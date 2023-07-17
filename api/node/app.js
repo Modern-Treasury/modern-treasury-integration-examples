5855d7fba6041858438c1c3ba9d67bbe83fc3541
Charlie Gerrior <charles.gerrior@moderntreasury.com>
2023-07-17 17:02:05 -0400

    initial commit

--- api/node/app.js
+++ api/node/app.js
@@ -5,7 +5,13 @@ const bodyParser = require('body-parser');
 const modernTreasury = require('modern-treasury');
 const sessions = require('express-session');
 
+const fetch = require('node-fetch');
+
+const base64 = require('base-64');
+
 var randtoken = require('rand-token');
+
+
 
 require('dotenv').config()
 
@@ -56,7 +62,7 @@ app.post('/api/create-cp-acf', async function (req, res) {
 
     res.redirect('/embed.html');
   }
-  //}
+  
   catch (err) {
     console.log(err)
   }
@@ -82,17 +88,45 @@ app.post('/api/create-cp-pf', async function (req, res) {
 
     res.redirect('/embed.html');
   }
-  //}
+
+  catch (err) {
+    console.log(err)
+  }
+});
+
+// POST route to handle a new user onboarding form
+app.post('/api/create-uo', async function (req, res) {
+  try {
+    fetch("https://app.moderntreasury.com/api/user_onboardings", {
+      method: 'POST',
+      headers: new Headers({
+        'content-type': 'application/json',
+        "Authorization": `Basic ${base64.encode(`${MT_ORG_ID}:${MT_API_KEY}`)}`
+      }),
+      body: JSON.stringify({flow_alias: req.body['onboarding_type']})
+    }).then(response => {
+      if (!response.ok) throw new Error(response.status);
+      //return response.json();
+      session=req.session;
+      session.user_onboarding_id=response.json()["id"];
+    })
+
+
+
+    res.redirect('/uo_embed.html');
+  }
+
   catch (err) {
     console.log(err)
   }
 });
 
 
+
 // This endpoint provides configuration to modern-treasury-js
 app.get('/config', function (req, res) {
   res.setHeader('Content-Type', 'application/javascript');
-  res.send(`window.mtConfig = { publishableKey: '${PUB_KEY}',` + `clientToken: '${session.client_token}'` + `}`);
+  res.send(`window.mtConfig = { publishableKey: '${PUB_KEY}',` + `clientToken: '${session.client_token}'` + `userOnboardingId: + '${session.user_onboarding_id}'` `}`);
 });
 
 // Mounts express.static to render example forms
